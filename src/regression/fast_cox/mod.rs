ee08806c9834d20b18d4bd0d80d8a977aa5335c8
Cameron Lyons <cameron@Mac-2522.lan>
2026-06-30 13:46:49 -0500

    Share row-major matrix standardization

--- src/regression/fast_cox/mod.rs
+++ src/regression/fast_cox/mod.rs
@@ -2,6 +2,7 @@ use ndarray::{ArrayView1, ArrayView2};
 use pyo3::prelude::*;
 use rayon::prelude::*;
 
+use crate::internal::matrix::standardize_row_major_matrix;
 use crate::{SurvivalError, SurvivalResult};
 
 include!("types.rs");
