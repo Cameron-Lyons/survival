456ad36bf37c1763d237eeb5e7e967e7d25c9309
Cameron Lyons <cameron@Mac-2522.lan>
2026-06-30 15:17:00 -0500

    Skip zero-weight Cox exponentials

--- src/regression/fast_cox/mod.rs
+++ src/regression/fast_cox/mod.rs
@@ -2,7 +2,9 @@ use ndarray::{ArrayView1, ArrayView2};
 use pyo3::prelude::*;
 use rayon::prelude::*;
 
-use crate::internal::cox_risk::{cox_risk_shift, precompute_cox_risk_set_cumsum, shifted_exp_eta};
+use crate::internal::cox_risk::{
+    cox_risk_shift, precompute_cox_risk_set_cumsum, shifted_exp_eta, shifted_exp_eta_with_shift,
+};
 use crate::internal::matrix::{
     standardize_or_borrow_row_major_matrix, standardize_row_major_matrix,
 };
