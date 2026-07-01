92dd6511147f79999ab595860d59b0fe490d4e10
Cameron Lyons <cameron@Mac-2650.lan>
2026-07-01 12:11:48 -0500

    Reuse fast Cox predictor buffers

--- src/regression/fast_cox/mod.rs
+++ src/regression/fast_cox/mod.rs
@@ -3,7 +3,7 @@ use pyo3::prelude::*;
 use rayon::prelude::*;
 
 use crate::internal::cox_risk::{
-    cox_risk_shift, precompute_cox_risk_set_cumsum, shifted_exp_eta, shifted_exp_eta_with_shift,
+    cox_risk_shift, precompute_cox_risk_set_cumsum, shifted_exp_eta_with_shift,
 };
 use crate::internal::matrix::{
     standardize_or_borrow_row_major_matrix, standardize_row_major_matrix,
